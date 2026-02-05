2f1fa695f47ee849d90933dac3f7b031809e53d0
VISORKILLEN <viktor.gustavsson@live.se>
2026-02-05 11:02:22 +0100

    Made new Consoleapp because the other one stopped working

---
+++ Individuellt databasproject V2/AdoNetFunctions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Individuellt_databasproject_V2
+{
+    internal class AdoNetFunctions
+    {
+
+        private static string connectionString = "Server=localhost;Database=Final_School_App;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public static void ShowStaff(string position = null)
+        {
+            using SqlConnection connection = new SqlConnection(connectionString);
+
+            // Create command to execute stored procedure
+            SqlCommand cmd = new SqlCommand("GetStaff", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            // Add position parameter if provided
+            if (!string.IsNullOrEmpty(position))
+            {
+                cmd.Parameters.AddWithValue("@Position", position);
+            }
+
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+
+            // Print staff details
+            Console.WriteLine("All personal:");
+            while (reader.Read())
+            {
+                Console.WriteLine($"Namn: {reader["FirstName"]} {reader["LastName"]}," +
+                    $"Position: {reader["Position"]}, " +
+                    $"Avdelning: {reader["Department"]}, " +
+                    $"Lön: {reader["Salary"]} KR, " +
+                    $"År som anställd på skolan: {reader["YearsAtSchool"]}\n");
+            }
+        }
+
+
+    }
+}
